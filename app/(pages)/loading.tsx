acbc0a56aec96b871bcce03edd7bbcb38a9c1c38
Daniel Emil <danielemilsig@gmail.com>
2025-02-19 02:08:01 +0000

    add loading file

---
+++ app/(pages)/loading.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from "@heroui/spinner";
+
+const Loading = () => {
+  return (
+    <div className="w-full h-20 flex justify-center items-center">
+      <Spinner />
+    </div>
+  );
+};
+
+export default Loading;
